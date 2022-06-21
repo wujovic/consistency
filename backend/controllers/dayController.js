fd57fe4c06d7bfff3fa45f91d1489eee29927f4d
wujovic <veljkovujovic185@gmail.com>
2022-06-21 23:18:36 +0200

    error handling

--- backend/controllers/dayController.js
+++ backend/controllers/dayController.js
@@ -1,30 +1,36 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get Days
 // @route GET /api/days
 // @access Private
-const getDays = (req, res) => {
+const getDays = asyncHandler (async (req, res) => {
     res.status(200).json({message: 'Get Days'})
-}
+})
 
 // @desc Set Day
 // @route POST /api/days
 // @access Private
-const setDay = (req, res) => {
+const setDay = asyncHandler (async (req, res) => {
+   if(!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field.')
+   }
     res.status(200).json({message: 'Set Day'})
-}
+})
 
 // @desc Update Day
 // @route PUT /api/days/:id
 // @access Private
-const updateDay = (req, res) => {
+const updateDay = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Update Day ${req.params.id}`})
-}
+})
 
 // @desc Delete Goal
 // @route DELETE /api/days/:id
 // @access Private
-const deleteDay = (req, res) => {
+const deleteDay = asyncHandler (async (req, res) => {
     res.status(200).json({message: `Delete Day ${req.params.id}`})
-}
+})
 
 module.exports = {
     getDays, 
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 4000
 const app = express()
+const {errorHandler} = require('../backend/middleware/errorMiddleware')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.use('/api/days', require('./routes/dayRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
