0c7eea01443d6a2d7f16480dbaf291b35ae87976
wujovic <veljkovujovic185@gmail.com>
2022-07-06 01:54:16 +0200

    user days protection

--- backend/routes/dayRoutes.js
+++ backend/routes/dayRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const {getDays, setDay, updateDay, deleteDay} = require('../controllers/dayController')
+const {protect} = require('../middleware/authMiddleware')
 
-router.route('/').get(getDays).post(setDay)
-router.route('/:id').put(updateDay).delete(deleteDay)
+router.route('/').get(protect, getDays).post(protect, setDay)
+router.route('/:id').put(protect, updateDay).delete(protect, deleteDay)
 
 module.exports = router
